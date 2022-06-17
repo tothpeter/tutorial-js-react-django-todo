fc339169ec91d5e9c0bf438d150b4822e807f2e2
Peter Toth <tothpeter08@gmail.com>
2022-06-16 23:49:00 -0600

    Create the todo model

--- api/todo/models.py
+++ api/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+
+
+    def __init__(self, arg):
+        super(Todo, self).__init__()
+        self.arg = arg
