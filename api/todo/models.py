1165c63dd9e642814fb791aae7e8970ce6097cfd
Peter Toth <tothpeter08@gmail.com>
2022-06-17 00:33:36 -0600

    Add todos endpoints

--- api/todo/models.py
+++ api/todo/models.py
@@ -4,8 +4,3 @@ class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
-
-
-    def __init__(self, arg):
-        super(Todo, self).__init__()
-        self.arg = arg
